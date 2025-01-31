5db0e733d40d395c33f6d067e74a6176f72b0b16
81784244368 <paulo.silva-junior@serpro.gov.br>
2025-01-31 08:30:44 -0300

    atualizado build gradle target sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.pjlaapps.maisumplano"
         minSdk = 33
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
